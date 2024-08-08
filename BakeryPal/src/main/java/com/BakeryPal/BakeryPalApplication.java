a822335fd9f8ad64c02767c67748c5b7e83e768d
Khushmita Kaur <kkaur35@gatech.edu>
2024-08-08 14:21:53 -0700

    added project structure

---
+++ BakeryPal/src/main/java/com/BakeryPal/BakeryPalApplication.java
@@ -0,0 +1,13 @@
+package com.BakeryPal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BakeryPalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BakeryPalApplication.class, args);
+	}
+
+}
