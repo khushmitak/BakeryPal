ce6fc3774b6423934418fef0234df25c826715f9
Khushmita Kaur <kkaur35@gatech.edu>
2024-08-08 17:48:47 -0700

    added model folder

---
+++ BakeryPal/src/main/java/com/BakeryPal/model/BakeryItem.java
@@ -0,0 +1,30 @@
+package com.BakeryPal.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "bakeryitem")
+public class BakeryItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ItemID")
+    private int itemID;
+
+    @Column(name = "ItemName")
+    private String itemName;
+
+    @Column(name = "Description")
+    private String description;
+
+    @Column(name = "Price")
+    private float price;
+
+    @Column(name = "CategoryName")
+    private String categoryName;
+}
